4bbd11ffdfa4d0ae4a688f7a6828ba7cf782830f
MhmdSalim <MhmdSalim@gmail.com>
2026-05-08 17:50:00 +0300

    update req.txt

--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\Dell\OneDrive - Beirut Arab University\Desktop\AgentForge\agentforge\venv
+set VIRTUAL_ENV=C:\Users\1mhmd\OneDrive\Desktop\Ai Projects\AgentForge\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
